60413d787d889cf0d6168aed25c7506648ec0a3d
Kevin Lamothe <klamothe91@gmail.com>
2018-01-22 23:12:48 -0600

    Created integer arrays for each GPO port. Switch cases for hours and minute functions to set bits. Currently not tested.

--- WordClock.ino
+++ WordClock.ino
@@ -5,143 +5,284 @@
 void setup()
 {
   Serial.begin(9600);
-    while (!Serial) ; // wait for serial
+  while (!Serial) ; // wait for serial
   delay(200);
   Serial.println("Word Clock Debugging");
   Serial.println("-------------------");
 	
 	Wire.begin(); // wake up I2C bus
-				  // set I/O pins to outputs
+  // set I/O pins to outputs
 	Wire.beginTransmission(0x20);
 	Wire.write(0x00); // IODIRA register
 	Wire.write(0x00); // set all of port A to outputs
 	Wire.endTransmission();
+ 
 	Wire.beginTransmission(0x20);
 	Wire.write(0x01); // IODIRB register
 	Wire.write(0x00); // set all of port B to outputs
+  Wire.endTransmission();
 }
 
 void loop()
 {
-  
-	int exampel = GetIntegerTime();
-	Serial.println(exampel);
-  delay(1000);
+	int hour = EncodedHour();
+  int minute = EncodedMinute();
 
+  int GPOA0 [8] = {0,0,0,0,0,0,1,1};
+  int GPOB0 [8] = {0,0,0,0,0,0,0,0};
+  int GPOA1 [8] = {0,0,0,0,0,0,0,0};
+
+  //Swtich-Case for setting bits corresponding to each hour
+  switch (hour)
+  {
+    case 10:
+      GPOA1[1] = 1;
+      break;
+
+    case 20:
+      GPOB0[4] = 1;
+      break;
+
+    case 30:
+      GPOB0[3] = 1;
+      break;
+
+    case 40:
+      GPOB0[2] = 1;
+      break;
+
+    case 50:
+      GPOB0[1] = 1;
+      break;
+
+    case 60:
+      GPOB0[0] = 1;
+      break;
+
+    case 70:
+      GPOA1[7] = 1;
+      break;
+
+    case 80:
+      GPOA1[6] = 1;
+      break;;
+
+    case 90:
+      GPOA1[5] = 1;
+      break;;
+
+    case 100:
+      GPOA1[4] = 1;
+      break;
+
+    case 110:
+      GPOA1[3] = 1;
+      break;
+
+    case 120:
+      GPOA1[2] = 1;
+      break;
+  }
+
+  //Swtich-Case for setting bits corresponding to each minute
+  switch (minute)
+  {
+    case 0:
+      GPOA1[3] = 1; //Oclock
+      break;
+
+    case 1:
+      GPOA1[2] = 1; //Five
+      GPOA1[4] = 1; //Past
+      break;
+
+    case 2:
+      GPOA0[1] = 1; //Ten
+      GPOA1[4] = 1; //Past
+      break;
+
+    case 3:
+      GPOA1[0] = 1; //Quarter
+      GPOA1[4] = 1; //Past
+      break;
+
+    case 4:
+      GPOB0[7] = 1; //Twenty
+      GPOA1[4] = 1; //Past
+      break;
+
+    case 5:
+      GPOB0[6] = 1; //Half
+      GPOA1[4] = 1; //Past
+      break;
+
+    case 6:
+      GPOB0[7] = 1; //Twenty
+      GPOB0[5] = 1; //Minutes
+      GPOA0[5] = 1; //To
+      break;
+
+    case 7:
+      GPOA0[0] = 1; //Quarter
+      GPOA0[5] = 1; //To
+      break;
+
+    case 8:
+      GPOA0[1] = 1; //Ten
+      GPOB0[5] = 1; //Minutes
+      GPOA0[5] = 1; //To
+      break;
+
+    case 9:
+      GPOA0[2] = 1; //Five
+      GPOB0[5] = 1; //Minutes
+      GPOA0[5] = 1; //To
+      break;
+  }
   
-	
-	/*
-	Wire.beginTransmission(0x20);
-	Wire.write(0x20); //GPIOA
-	Wire.write(0b10101010 ); 
-	Wire.endTransmission();
-  Serial.print("Exicuted");
-  */
+ for(int i = 7; i > 0; i--)
+{
+  Serial.print(GPOA0[i]);
+}
+Serial.print("|");
+
+ for(int i = 7; i > 0; i--)
+{
+  Serial.print(GPOB0[i]);
+}
+Serial.print("|");
+ for(int i = 7; i > 0; i--)
+{
+  Serial.print(GPOA1[i]);
+}
+ Serial.println();
+delay(5000);
+
+
+  Wire.beginTransmission(0x20);
+  Wire.write(0x12); // GPIOA
+  Wire.write(0b11111111); // port A
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(0x20);
+  Wire.write(0x13); // GPIOB
+  Wire.write(0b11111111); // port B
+  Wire.endTransmission();
+
 
 }
 
 //------------------------------------------------------------------
 //Function to read RTC and return an integer value for each hour
-//and minute combination possible. Negative values indicate
-//some sort of failure
+//Negative values indicate some sort of failure
 //------------------------------------------------------------------
-int GetIntegerTime()
+int EncodedHour()
 {
   tmElements_t tm;
-  int EncodedTime;
+  int EncodedHours;
 
   //Reding and setting hour
   if (RTC.read(tm)) 
   {
     if(tm.Hour == 0 || tm.Hour == 12)
-    { EncodedTime = 10; }
+    { EncodedHours = 10; }
     
     else if(tm.Hour == 1 || tm.Hour == 13)
-    { EncodedTime = 20; }
+    { EncodedHours = 20; }
     
     else if(tm.Hour == 2 || tm.Hour == 14)
-    { EncodedTime = 30; }
+    { EncodedHours = 30; }
 
     else if(tm.Hour == 3 || tm.Hour == 15)
-    { EncodedTime = 40; }
+    { EncodedHours = 40; }
 
     else if(tm.Hour == 4 || tm.Hour == 16)
-    { EncodedTime = 50; }
+    { EncodedHours = 50; }
 
     else if(tm.Hour == 5 || tm.Hour == 17)
-    { EncodedTime = 60; }
+    { EncodedHours = 60; }
 
     else if(tm.Hour == 6 || tm.Hour == 18)
-    { EncodedTime = 70; }
+    { EncodedHours = 70; }
 
     else if(tm.Hour == 7 || tm.Hour == 19)
-    { EncodedTime = 80; }
+    { EncodedHours = 80; }
 
     else if(tm.Hour == 8 || tm.Hour == 20)
-    { EncodedTime = 90; }
+    { EncodedHours = 90; }
 
     else if(tm.Hour == 9 || tm.Hour == 21)
-    { EncodedTime = 100; }
+    { EncodedHours = 100; }
     
     else if(tm.Hour == 10 || tm.Hour == 22)
-    { EncodedTime = 110; }
+    { EncodedHours = 110; }
 
     else if(tm.Hour == 11 || tm.Hour == 23)
-    { EncodedTime = 120; }
+    { EncodedHours = 120; }
     
     else
-    { EncodedTime = -300; }
+    { EncodedHours = -300; }
   }
+    
+  return EncodedHours;
+}
 
+//------------------------------------------------------------------
+//Function to read RTC and return an integer value for each minute
+//Negative values indicate some sort of failure
+//------------------------------------------------------------------
+int EncodedMinute()
+{
+  tmElements_t tm;
+  int EncodedMinutes = 0;
+  
    //Reading and setting minutes
    if (RTC.read(tm)) 
    {
     //O'clock
     if(tm.Minute > 0 && tm.Minute <= 5)
-    { EncodedTime = EncodedTime; }
+    { EncodedMinutes = EncodedMinutes; }
 
     //Five past the hour
     else if(tm.Minute > 5  && tm.Minute <= 10)
-    { EncodedTime = EncodedTime + 1; }
+    { EncodedMinutes = EncodedMinutes + 1; }
 
     //Ten past the hour
     else if(tm.Minute > 10 && tm.Minute <= 15)
-    { EncodedTime = EncodedTime + 2; }
+    { EncodedMinutes = EncodedMinute + 2; }
 
     //Quarter past the hour
     else if(tm.Minute > 15 && tm.Minute <= 20)
-    { EncodedTime = EncodedTime + 3; }
+    { EncodedMinutes = EncodedMinutes + 3; }
 
     //Twenty past the hour
     else if(tm.Minute > 20 && tm.Minute <= 30)
-    { EncodedTime = EncodedTime + 4; }
+    { EncodedMinutes = EncodedMinutes + 4; }
 
     //Half past the hour
     else if(tm.Minute > 30 && tm.Minute <= 40)
-    { EncodedTime = EncodedTime + 5; }
+    { EncodedMinutes = EncodedMinutes + 5; }
 
     //Twenty to the next hour
     else if(tm.Minute > 40 && tm.Minute <= 45)
-    { EncodedTime = EncodedTime + 6; }
+    { EncodedMinutes = EncodedMinutes + 6; }
 
     //Quarter to the next hour
     else if(tm.Minute > 45 && tm.Minute <= 50)
-    { EncodedTime = EncodedTime + 7; }
+    { EncodedMinutes = EncodedMinutes + 7; }
 
     //Ten to the next hour
     else if(tm.Minute > 50 && tm.Minute <= 55)
-    { EncodedTime = EncodedTime + 8; }
+    { EncodedMinutes = EncodedMinutes + 8; }
 
     //Five to the next hour
     else if(tm.Minute > 55)
-    { EncodedTime = EncodedTime + 9; }
+    { EncodedMinutes = EncodedMinutes + 9; }
     
     else
-    { EncodedTime = -400; }
+    { EncodedMinutes = -400; }
   }
     
-  return EncodedTime;
+  return EncodedMinutes;
 }
-
-
 
